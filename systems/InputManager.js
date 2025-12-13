dd78634b36bcac1e0242b725f363e757ce5b5d06
Pachodev77 <pachodev77@gmail.com>
2025-12-13 16:57:14 -0500

    btns glss

--- systems/InputManager.js
+++ systems/InputManager.js
@@ -21,8 +21,8 @@ export class InputManager {
                 zone: moveZone,
                 mode: 'static',
                 position: { left: '50%', top: '50%' },
-                color: 'orange',
-                restOpacity: 1
+                color: '#00f3ff',
+                restOpacity: 0.8
             });
 
             moveJoystick.on('move', (evt, data) => {
@@ -40,8 +40,8 @@ export class InputManager {
                 zone: cameraZone,
                 mode: 'static',
                 position: { left: '50%', top: '50%' },
-                color: 'orange',
-                restOpacity: 1
+                color: '#00f3ff',
+                restOpacity: 0.8
             });
 
             cameraJoystick.on('move', (evt, data) => {
